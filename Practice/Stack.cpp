11fa8a882bf9d4cf4e5f93b00815d48fb3d35a7d
JLibyvali <1954925228@qq.com>
2024-03-17 22:32:04 +0800

    DFS and BFS

--- Practice/Stack.cpp
+++ Practice/Stack.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stack>
 #include <vector>
+#include <algorithm>
+#include <string>
 
 
 using namespace std;
@@ -61,30 +63,91 @@ using namespace std;
 
 
 // Hanota
-stack<int> S[3];
-void move(int x, int y) {
+//stack<int> S[3];
+//void move(int x, int y) {
+//
+//	int  tmp = S[x].top();
+//	S[x].pop();
+//
+//	S[y].push(tmp);
+//
+//	cout<< x <<"--->"<< y <<endl;
+//
+//}
+//
+//void hanoi(int A, int B, int C, int n) {
+//
+//	if(n == 1) {
+//		move(A,C);
+//		return;
+//	}
+//
+//	hanoi(A,C,B,n-1);
+//	move(A,C);
+//	hanoi(B,A,C,n-1);
+//
+//}
 
-	int  tmp = S[x].top();
-	S[x].pop();
 
-	S[y].push(tmp);
 
-	cout<< x <<"--->"<< y <<endl;
+// Recuresion 2
+//
+//long long f(long long _x, long long _y, long long _p){
+//
+//	if(_y == 0){
+//		return 1 % _p;
+//	}else if(_y % 2 == 0){
+//		long long tmp = f(_x,_y / 2, _p);
+//		return tmp*tmp % _p;
+//	}else {
+//
+//		long long tmp = f(_x,_y / 2 , _p);
+//		return (tmp*tmp * _x) % _p;
+//	}
+//
+//}
 
-}
 
-void hanoi(int A, int B, int C, int n) {
 
-	if(n == 1) {
-		move(A,C);
-		return;
-	}
+// Rec 3
+//int ans[205],a[205],b[205];
+//bool vis[205];
+//int n;
+//
+//int f(int _x){
+//
+//
+//	if( _x >= n){
+//		return 0;
+//	}
+//
+//	if( vis[_x]){
+//		return ans[_x];
+//	}
+//
+//	return ans[_x] = min(f(_x + a[_x]) , f(_x + a[_x])) + 1;
+//
+//}
 
-	hanoi(A,C,B,n-1);
-	move(A,C);
-	hanoi(B,A,C,n-1);
+// Rec4
 
-}
+//int gcd(int _x , int _y) {
+//
+//
+//	if(_y == 0) {
+//		return _x;
+//
+//	} else {
+//		return gcd(_y, _x % _y);
+//	}
+//
+//}
+
+
+
+
+
+
 
 int  main() {
 
@@ -173,11 +236,94 @@ int  main() {
 //		S[2].pop();
 //
 //	}
-/*
-Input: Hanoi least steps.
 
-*/
 
+	/*
+	Input: t,x,y,p; calculate 'x^y'
+	Output: 'x^y' % p;
+
+	*/
+
+//	int t;
+//	long long x,y,p;
+//	cin>>t;
+//
+//	while(t){
+//		cin>>x>>y>>p;
+//		cout<<f(x,y,p)<<endl;
+//		t --;
+//	}
+
+//	cin>>n;
+//	for(int i=0;i<n;i++){
+//		cin>>a[i];
+//	}
+//	for(int i=0;i<n;i++){
+//		cin>> b[i];
+//	}
+//
+//
+//	cout<<f(0)<<endl;
+
+
+
+	/*
+	Input : x;
+	Output: Max Gongyueshu
+
+	*/
+
+//	int t;
+//	int x, y;
+//	cin>>t;
+//	while(t) {
+//
+//		cin>>x>>y;
+//		cout<<gcd(x,y)<<endl;
+//		t--;
+//
+//	}
+
+
+//	int n;
+//	string op;
+//	stack<string> s1,s2;
+//	cin>>n;
+//
+//	for(int i=0; i<n; i++) {
+//		cin>>op;
+//
+//		if(op[0] == 'V') {
+//			string tmp;
+//			cin>>tmp;
+//			while(!s2.empty()) {
+//				s2.pop();
+//			}
+//			
+//			s1.push(tmp);
+//
+//		} else if(op[0] == 'B') {
+//
+//			if(s1.size() <= 1) {
+//				cout<<"Ignore"<<endl;
+//				continue;
+//			}
+//
+//			s2.push(s1.top());
+//			s1.pop();
+//
+//		} else {
+//			if(s2.empty()) {
+//				cout<<"Ignore"<<endl;
+//				continue;
+//			}
+//			
+//			s1.push(s2.top());
+//			s2.pop();
+//		}
+//		
+//		cout<<s1.top()<<endl;
+//	}
 
 
 	return 0;
